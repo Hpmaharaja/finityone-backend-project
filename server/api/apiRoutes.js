e3ba78768f2dbf3393a1fc5c3aa7d33979657704
Heran Patel <radharani@Herans-MacBook-Pro.local>
2019-06-07 19:51:26 -0700

    added working endpoiint

--- server/api/apiRoutes.js
+++ server/api/apiRoutes.js
@@ -3,13 +3,12 @@
 // =============================================================================
 var express = require('express'),
     apiRoutes = express(),
-    bodyParser = require('body-parser'),
-    con = require('../db');
+    bodyParser = require('body-parser');
 
 apiRoutes.use(bodyParser.json());
 apiRoutes.use(bodyParser.urlencoded({extended: true}));
 
-var emailRoutes = require('./emailController');
+var emailRoutes = require('./email/emailController');
 apiRoutes.use('/email', emailRoutes);
 
 module.exports = apiRoutes;
